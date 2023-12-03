b40792e403464fce0b318d509aa6db97aad5055b
asfandtaimoor <asfandtaimoor786@gmail.com>
2023-12-03 21:29:31 +0500

    Modal updated

--- src/components/Common/header.jsx
+++ src/components/Common/header.jsx
@@ -41,63 +41,7 @@ function Header() {
     </div>
   );
 }
-
 export default Header;
-
-// function Modals() {
-//   const [show, setShow] = useState(true);
-
-//   const handleClose = () => setShow(false);
-//   const handleShow = () => setShow(true);
-
-//   return (
-//     <>
-//       <Button variant="primary" onClick={handleShow}>
-//         reset password button
-//       </Button>
-
-//       <Modal
-//         show={show}
-//         onHide={() => setShow(false)}
-//         size="lg"
-//         aria-labelledby="contained-modal-title-vcenter"
-//         centered
-//       >
-//         <Modal.Header className="border-0" closeButton></Modal.Header>
-//         <Modal.Body>
-//           <h1 className="ts-heading-font fw-bold text-uppercase ts-fs-40 ts-text-gray-6 text-center mb-08">
-//             reset password
-//           </h1>{" "}
-//           <Form className="mb-5">
-//             <Form.Group className="mb-4">
-//               <Form.Label className="ts-text-gray-5 ts-fs-22 fw-medium">
-//                 Old Password
-//               </Form.Label>
-//               <Form.Control size="lg" type="password" />
-//             </Form.Group>
-//             <Form.Group className="mb-4">
-//               <Form.Label className="ts-text-gray-5 ts-fs-22 fw-medium">
-//                 New Password
-//               </Form.Label>
-//               <Form.Control size="lg" type="password" />
-//             </Form.Group>
-//             <Form.Group className="mb-4">
-//               <Form.Label className="ts-text-gray-5 ts-fs-22 fw-medium">
-//                 Confirm New Password
-//               </Form.Label>
-//               <Form.Control size="lg" type="password" />
-//             </Form.Group>
-//           </Form>
-//           <div className="text-center">
-//             <button class="btn ts-btn ts-btn--lg ts-fs-22 fw-bold ts-btn-primary text-uppercase">
-//               update password
-//             </button>
-//           </div>
-//         </Modal.Body>
-//       </Modal>
-//     </>
-//   );
-// }
 
 function Modals() {
   const [showResetPassword, setShowResetPassword] = useState(false);
@@ -174,9 +118,81 @@ function Modals() {
 
       {/* Login Modal */}
       <Modal
-        // show={showLogin}
-        show={true}
+        show={showLogin}
         onHide={handleCloseLogin}
+        size="md"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header className="border-0" closeButton></Modal.Header>
+        <Modal.Body>
+          {/* ... (Login form structure) */}
+          <h1 className="ts-heading-font fw-bold text-uppercase ts-fs-40 ts-text-gray-6 text-center mb-08">
+            creat new account
+          </h1>{" "}
+          <Form className="mb-5">
+            <Form.Group className="mb-4">
+              <Form.Label className="ts-text-gray-5 ts-fs-22 fw-medium">
+                Email Address
+              </Form.Label>
+              <Form.Control size="lg" type="email" />
+            </Form.Group>
+
+            <Form.Group className="mb-4">
+              <div className="d-flex justify-content-between">
+                <Form.Label className="ts-text-gray-5 ts-fs-22 fw-medium">
+                  Password
+                </Form.Label>
+                {/* Forget Password Button */}
+                <Button
+                  className="ts-link"
+                  href="#"
+                  variant="link"
+                  onClick={handleShowResetPassword}
+                >
+                  Forgot Password?
+                </Button>
+              </div>
+              <Form.Control size="lg" type="password" />
+            </Form.Group>
+          </Form>
+          <div className="text-center">
+            <button class="btn ts-btn ts-btn--lg ts-fs-22 fw-bold ts-btn-primary text-uppercase mb-5">
+              Continue
+            </button>
+          </div>
+          <div class="ts-separator mb-5">
+            <div class="line "></div>
+            <h2 className="text-uppercase ts-fs-22 fw-bold ts-text-gray-6 mb-0 mx-3">
+              or log in with
+            </h2>
+            <div class="line"></div>
+          </div>
+          <div className="mb-07">
+            <ModalSocialMedia />
+          </div>
+          <p className="text-center">
+            Don’t have an Account?
+            <Button
+              className="p-0 ms-2"
+              variant="link"
+              onClick={handleShowCreateAccount}
+            >
+              Sign up here
+            </Button>
+          </p>
+        </Modal.Body>
+      </Modal>
+
+      {/* Create Account Button */}
+      <Button variant="primary" onClick={handleShowCreateAccount}>
+        Create Account Button
+      </Button>
+
+      {/* Create Account Modal */}
+      <Modal
+        show={showCreateAccount}
+        onHide={handleCloseCreateAccount}
         size="md"
         aria-labelledby="contained-modal-title-vcenter"
         centered
@@ -219,45 +235,31 @@ function Modals() {
             </Form.Group>
           </Form>
           <div className="text-center">
-            <button class="btn ts-btn ts-btn--lg ts-fs-22 fw-bold ts-btn-primary text-uppercase mb-07">
+            <button class="btn ts-btn ts-btn--lg ts-fs-22 fw-bold ts-btn-primary text-uppercase mb-5">
               Continue
             </button>
+          </div>
+          <div class="ts-separator mb-5">
+            <div class="line "></div>
+            <h2 className="text-uppercase ts-fs-22 fw-bold ts-text-gray-6 mb-0 mx-3">
+              or log in with
+            </h2>
+            <div class="line"></div>
           </div>
           <div className="mb-07">
             <ModalSocialMedia />
           </div>
           <p className="text-center">
-            Don’t have an Account?
+            Already have an account?
             <Button
               className="p-0 ms-2"
               variant="link"
-              onClick={handleShowCreateAccount}
+              onClick={handleShowLogin}
             >
-              Sign up here
+              Sign in here
             </Button>
           </p>
         </Modal.Body>
-      </Modal>
-
-      {/* Create Account Button */}
-      <Button variant="primary" onClick={handleShowCreateAccount}>
-        Create Account Button
-      </Button>
-
-      {/* Create Account Modal */}
-      <Modal
-        show={showCreateAccount}
-        onHide={handleCloseCreateAccount}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        {/* ... (Create account form structure) */}
-
-        {/* Login Button */}
-        <Button variant="link" onClick={handleShowLogin}>
-          Already have an account? Login
-        </Button>
       </Modal>
     </>
   );
