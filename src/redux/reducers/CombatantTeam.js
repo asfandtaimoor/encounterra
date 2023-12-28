157e93c895c650e5eb9e55781334a6b033bbf2e4
asfandtaimoor <asfandtaimoor786@gmail.com>
2023-12-28 15:08:03 +0500

    Combatant udpated

--- src/redux/reducers/CombatantTeam.js
+++ src/redux/reducers/CombatantTeam.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-toastify";
 
 const MAX_TEAM_SIZE = 6;
 
@@ -14,8 +15,18 @@ const CombatantTeam = createSlice({
 
       // Check if the team has reached the maximum size
       if (state[team].length >= MAX_TEAM_SIZE) {
-        console.warn(
-          `Cannot add more than ${MAX_TEAM_SIZE} items to ${team} team.`
+        // Show error toast
+        toast.error(
+          `Cannot add more than ${MAX_TEAM_SIZE} items to ${team} team.`,
+          {
+            position: "bottom-right",
+            autoClose: 3000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+          }
         );
         return state;
       }
@@ -26,6 +37,16 @@ const CombatantTeam = createSlice({
       if (!existingData) {
         // If data doesn't exist and team size is below the limit, push it into the team
         state[team].push(data);
+      } else {
+        toast.error(`Combatant is already in ${team} Team.`, {
+          position: "bottom-right",
+          autoClose: 3000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       }
 
       return state;
